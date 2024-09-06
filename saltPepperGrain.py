c4702ab4b4de2adf160fda5be6cfba6e4e54a6fe
chihiroprog <stutuart.barthe@gmail.com>
2024-09-06 16:55:13 +0200

    salt and pepper v1

---
+++ saltPepperGrain.py
@@ -0,0 +1,17 @@
+import skimage as ski
+import random as rd
+
+def pepperSaltGrain (inputImagePath):
+    image = ski.io.imread(inputImagePath)
+    for i in range(0, len(image)):
+        for j in range(0, len(image[i])):
+            image[i][j] = pepperSaltGrainEachPixel(image[i][j])
+    
+def pepperSaltGrainEachPixel(pixel: int)->int:
+    salt, pepper = False
+    if rd.randint(0, 1000) <= 2:
+        if  rd.randint(0, 1) == 0:
+            pixel = 0 # salt
+        else:
+            pixel = 255 # pepper   
+    return pixel
