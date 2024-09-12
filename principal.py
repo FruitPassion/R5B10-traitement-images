22a440859a40d33b3dab42f70112fc3b94478fb9
raphael <gamehotdl@gmail.com>
2024-09-12 09:49:14 +0200

    Refactor code to handle different types of noise and save the resulting image with a unique name

--- principal.py
+++ principal.py
@@ -3,26 +3,44 @@ from skimage import io, img_as_float, img_as_ubyte
 import matplotlib.pyplot as plt
 from src.bruit_additif import bruit_additif
 from src.bruit_multiplicatif import bruit_multiplicatif
+from src.saltPepperGrain import pepperSaltGrainEachPixel
+from time import time
+import sys
 
+arg = sys.argv[1]
+if arg not in ['additif', 'multiplicatif', 'poivre_et_sel']:
+    print("Argument invalide")
+    sys.exit()
+print(arg)
 
 image = io.imread('images_reference/image_reference1.png')
 image = img_as_float(image)
 
-mean = 0
-std_dev = 10
+mean = None
+std_dev = None
+if arg == 'additif':
+    mean = 0
+    std_dev = 10
+elif arg == 'mutiplicatif':
+    mean = 1
+    std_dev = 0.1
 
-# Créer une image bruitée avec du bruit multiplicatif
 noisy_image = np.zeros_like(image)
 
 for i in range(image.shape[0]):
     for j in range(image.shape[1]):
-        noisy_image[i, j] = bruit_additif(image[i, j], mean, std_dev)
-
+        if arg == 'poivre_et_sel':
+            noisy_image[i, j] = pepperSaltGrainEachPixel(image[i, j])
+        elif arg == 'additif':
+            noisy_image[i, j] = bruit_additif(image[i, j], mean, std_dev)
+        elif arg == 'multiplicatif':
+            noisy_image[i, j] = bruit_multiplicatif(image[i, j], mean, std_dev)
 
 noisy_image = np.clip(noisy_image, 0, 1)
 noisy_image_uint8 = img_as_ubyte(noisy_image)
 
-io.imsave('out/image_bruitee_multiplicatif.jpg', noisy_image_uint8)
+name = f"out/image_bruitee_multiplicatif_{time()}.png"
+io.imsave(name, noisy_image_uint8)
 
 fig, ax = plt.subplots(1, 2, figsize=(10, 5))
 ax[0].imshow(image, cmap='gray')
@@ -30,7 +48,7 @@ ax[0].set_title("Image Originale")
 ax[0].axis('off')
 
 ax[1].imshow(noisy_image, cmap='gray')
-ax[1].set_title("Image avec Bruit Multiplicatif")
+ax[1].set_title(f"Image avec bruit {arg}")
 ax[1].axis('off')
 
 plt.show()
