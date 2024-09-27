038e179b1b68f9a126a51414cd5511076619eb61
chihiroprog <stutuart.barthe@gmail.com>
2024-09-27 16:53:38 +0200

    snr fonctionnel

--- src/snr.py
+++ src/snr.py
@@ -1,18 +1,20 @@
 from math import log10
 from skimage import img_as_float
 
-def get_snr(imageSignal, imageBruit):
+def get_snr(imageBruit, imageSignal):
     
     imageSignal = img_as_float(imageSignal)
     imageBruit = img_as_float(imageBruit)
+    imageBruit = imageBruit - imageSignal
     signal, bruit = 0, 0
+
     if imageSignal.shape != imageBruit.shape:
         raise ValueError("Les deux images doivent avoir la même taille")
    
     for i in range(imageSignal.shape[0]):
-        for j in range(imageSignal.shape[1]):
+        for j in range(imageSignal.shape[0]):
             signal += imageSignal[i, j]**2
             bruit += imageBruit[i, j]**2
           
-    print("signal : ", signal, "\n", "bruit : ", bruit)
+    # print("signal : ", signal, "\n", "bruit : ", bruit)
     return 10 * log10(signal/bruit)
